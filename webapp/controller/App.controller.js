815eb826833ee4106405495cd9e09e2cdf5af04d
AdityaPrt <adityapnmit@gmail.com>
2023-06-05 13:56:20 +0000

    my projectsapui5 first commit

---
+++ webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sap.btp.projectsapui5.controller.App", {
+        onInit() {
+        }
+      });
+    }
+  );
+  
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapbtp/projectsapui5/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
